835ee81ae5d9140f5f776e0ae97cb9b8f43d4103
John Abreu <abreuboom@johns-mbp.bc.edu>
2017-04-27 00:19:58 -0400

    uploaded files

---
+++ main.js
@@ -0,0 +1,21 @@
+var database = firebase.database().ref();
+
+
+function sendChat() {
+    var name = $("#name").val();
+    var message = $("#message").val();
+
+    database.push({
+        'name': name,
+        'message': message
+    });
+
+    $("#submit").click(function(){
+        sendChat();
+    });
+
+    database.on('child_added', function(dataRow){
+    var row = dataRow.val();
+    $("#feed").append("<p><b>" + row.name + "</b> says " + row.message + "</p>");
+})
+}
